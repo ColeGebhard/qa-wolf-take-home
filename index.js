fc2d0c487029833e4dbb27f40b573beb00386b2d
Cole <gebhardcole@gmail.com>
2023-12-27 16:14:11 -0700

    finalized code

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-// EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
-const { chromium } = require("playwright");
-const fs = require("fs")
-const ObjectsToCsv = require("objects-to-csv")
+const { chromium } = require('playwright');
+const ObjectsToCsv = require('objects-to-csv');
 
 async function saveHackerNewsArticles() {
-  // launch browser
+  // Launch browser
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  // go to Hacker News
-  await page.goto("https://news.ycombinator.com");
+  // Go to Hacker News
+  await page.goto('https://news.ycombinator.com');
+
+  // Wait for the table to be visible
+  const tableSelector = 'table#hnmain .athing';
+  await page.waitForSelector(tableSelector, { visible: true});
+
+  // Retrieve articles directly in the page context
+  // Query Selector used in portions
+  // Seperating All articles
+  // Then seperating articles by first ten, and finally the title/url
+  const articles = await page.evaluate((tableSelector) => {
+    const rows = document.querySelectorAll(tableSelector);
+    return Array.from(rows)
+      .slice(0, 10)
+      .map((row) => {
+        const titleElement = row.querySelector('td.title > span.titleline > a');
+        return titleElement
+          ? {
+              title: titleElement.innerText,
+              url: titleElement.href,
+            }
+          : null;
+      });
+  }, tableSelector);
+
+  // Used a node package to send a csv to top-articles in articles folder
+  // Reference https://www.npmjs.com/package/objects-to-csv
+  (async () => {
+    const csv = new ObjectsToCsv(articles);
+  
+    // Save to file:
+    await csv.toDisk('./articles/top-articles.csv');
+  })();
 }
 
 (async () => {
